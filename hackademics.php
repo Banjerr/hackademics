41a6d607ff2b455bf4813b1b5cb5188a2dfa4996
Ben Redden <benjamminredden@gmail.com>
2016-03-26 22:24:52 -0400

    initial commit

---
+++ hackademics.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Hackademics
+ * Version: 0.1-alpha
+ * Description: A developer friendly Wordpress learning management plugin
+ * Author: Ben Redden
+ * Author URI: http://countryfriedcoders.me
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: hackademics
+ * Domain Path: /languages
+ * @package Hackademics
+ */
